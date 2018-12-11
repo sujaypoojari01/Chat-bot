7b593ac54df32fe1e64da91b69171ffd4fb0a284
Sujay Poojari <sujaypoojari01@gmail.com>
2018-12-12 00:38:51 +0530

    firebase

--- Chat-Bot/src/environments/environment.prod.ts
+++ Chat-Bot/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB9Pia4k9gkwwWdBnltsgmP5n4KzUKOkRQ',
+    authDomain: 'chat-bot-d4e6a.firebaseapp.com',
+    databaseURL: 'https://chat-bot-d4e6a.firebaseio.com',
+    projectId: 'chat-bot-d4e6a',
+    storageBucket: 'chat-bot-d4e6a.appspot.com',
+    messagingSenderId: '668333948224'
+  }
 };
